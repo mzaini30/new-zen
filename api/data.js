7271bb6c525d32040a8499987d2cd3bcf777cf3d
mzaini30 <muhzaini30@gmail.com>
2021-05-18 05:43:45 +0800

    upload

---
+++ api/data.js
@@ -0,0 +1,7 @@
+const sql = 'https://excalibur.mabaiz.web.id/sql.php'
+
+const komentar = '06eff6933f1004da5614a47edad6bea9' // id, nama, email, blog, komentar, slug
+const konten = 'e8286ab0f7b2fa5e0ebf06ff33ba5312' // id, judul, slug, gambar, deskripsi, isi, label, tanggal
+let akun = 'bd6753a7be2017e4e41aa8a2cccea8ae' // id, username, password
+
+module.exports = {sql, komentar, konten, akun}
---
+++ api/sitemap.js
@@ -0,0 +1,31 @@
+const axios = require('axios')
+const qs = require('qs')
+const {sql, konten} = require('./data')
+module.exports = async (req, res) => {
+	let olah = []
+	async function ambilData(){
+		let data = await axios.post(sql, qs.stringify({
+			id: konten,
+			kunci: 'ambil-slug'
+		}))
+		data = data.data
+		olah = data
+	}
+	ambilData()
+	if (olah) {
+		res.writeHead(200, {
+			"Content-Type": "image\/svg+xml"
+		})
+		let isi = `<urlset xmlns="http:\/\/www.sitemaps.org\/schemas\/sitemap\/0.9" xmlns:xhtml="http:\/\/www.w3.org\/1999\/xhtml">`
+		for (x of olah){
+			isi += `
+				<url>
+					<loc>https:\/\/www.catatanzen.my.id\/${x.slug}<\/loc>
+				<\/url>
+			`
+		}
+		isi += "<\/urlset>"
+		res.write(isi)
+		res.end()
+	}
+}
